110a0e7269fa6e3d9cb2bb639cc0717aaa9e77c9
Maksims0230 <maksims023@gmail.com>
2021-04-29 18:21:35 +0300

    first commit

---
+++ WPF NET 6/FontSizeConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WPF_NET_6
+{
+    public class FontSizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //18 - начальный шрифт - 450 высота
+            //200 - минимальная высота
+            //if (value != null) 
+            //    return (object)(450.0 / 18 * (double) value);
+            return (object)18;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WPF NET 6/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_NET_6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowMove_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                this.DragMove();
+            else if (e.RightButton == MouseButtonState.Pressed)
+            {
+
+            }
+        }
+
+        private void ButtonBaseMinimize_OnClick(object sender, RoutedEventArgs e) =>
+            this.WindowState = WindowState.Minimized;
+
+
+        private void ButtonBaseMaximize_OnClick(object sender, RoutedEventArgs e) =>
+            this.WindowState = this.WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
+
+        private void ButtonBaseClose_OnClick(object sender, RoutedEventArgs e) => this.Close();
+    }
+}
