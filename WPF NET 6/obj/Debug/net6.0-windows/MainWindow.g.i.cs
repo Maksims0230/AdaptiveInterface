de55f179092063cb76199055712940793ac41cc8
Maksims0230 <maksims023@gmail.com>
2021-04-29 18:30:45 +0300

    first commit

--- WPF NET 6/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ WPF NET 6/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -65,7 +65,7 @@ namespace WPF_NET_6 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WPF NET 6;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WPF NET 6;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
